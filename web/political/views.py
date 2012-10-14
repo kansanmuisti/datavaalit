3be54228892c308f3ae41861a37f3b10fb84928c
Juha Yrjölä <juha.yrjola@iki.fi>
2012-10-15 00:39:21 +0300

    candidate_social_feeds: Display stats

--- web/political/views.py
+++ web/political/views.py
@@ -4,5 +4,9 @@ from social.models import *
 from political.models import *
 
 def show_candidates_social_feeds(request):
-    return render_to_response('political/candidate_social_feeds.html',
+    feed_count = CandidateFeed.objects.count()
+    update_count = Update.objects.count()
+    last_update = CandidateFeed.objects.filter(last_update__isnull=False).order_by('-last_update')[0].last_update
+    args = dict(feed_count=feed_count, update_count=update_count, last_update=last_update)
+    return render_to_response('political/candidate_social_feeds.html', args,
                               context_instance=RequestContext(request))
