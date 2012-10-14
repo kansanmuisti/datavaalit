3d582135b3fd7f8615ca392958c27047e45354a0
Joona Lehtomaki <joona.lehtomaki@gmail.com>
2012-10-14 22:25:02 +0300

    Added SQL scripts to query the database

---
+++ sql/total_expense_per_party.sql
@@ -0,0 +1,14 @@
+﻿SELECT 
+  SUM(public.political_expense.sum),
+  COUNT(public.political_candidate.id),
+  public.political_party.name 
+FROM 
+  public.political_party, 
+  public.political_candidate, 
+  public.political_expense
+WHERE 
+  political_candidate.party_id = political_party.id AND
+  political_expense.candidate_id = political_candidate.id AND
+  political_expense.expense_type_id = 7
+GROUP BY
+  public.political_party.id;
